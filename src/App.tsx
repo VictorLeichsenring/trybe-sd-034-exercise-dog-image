d7bdca72dde89d9a2cefbf0af658cc46b03cf350
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-09-01 17:06:42 -0300

    Criado estrutura e chamada api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [imageUrl, setImageUrl] = useState('');
+
+  const fetchDog = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((res) => res.json())
+      .then((data) => setImageUrl(data.message))
+      .then(() => setIsLoading(false));
+  };
+
+  useEffect(() => {
+    fetchDog();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
   return (
     <div>
-      <h1>Doguinhos</h1>
+      <h1>Gerador de Dog</h1>
+      <h2>O seu doguinho é:</h2>
+      <img src={ imageUrl } alt="Doguinho aleatório" />
+      <button onClick={ fetchDog }>Novo doguinho!</button>
     </div>
   );
 }
